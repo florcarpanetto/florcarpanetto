bf50da4a357543247f1daa0d286e3c3b5f1b3971
Flor Carpanetto <florcarpanetto@gmail.com>
2023-04-11 16:12:04 -0300

    animations added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import { motion } from 'framer-motion';
 import backgroungImage from './assets/Grayscale_LOGO 1280x1024_72dpi.png';
 import isoLogo from './assets/isologo.png';
 import logo from './assets/logo-color.png';
@@ -18,20 +19,75 @@ function App() {
 
   return (
     <>
-      <img src={backgroungImage} alt='imagen logo fondo' className='background-image' />
+      <motion.img
+        src={backgroungImage}
+        alt='imagen logo fondo'
+        className='background-image'
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 0.3 }}
+        transition={{ duration: 3 }}
+
+      />
       <header>
         <div className='header-container'>
 
 
-          <img src={logo} alt='Logo color' className='logo' />
-          <img src={isoLogo} alt='Flor Carpanetto' className='isologo' />
+          <motion.img
+            src={logo}
+            alt='Logo color'
+            className='logo'
+            animate={{
+              scale: [1, 1.5, 1.5, 1, 1],
+              rotate: [0, 0, 270, 270, 0]
+            }}
+          />
+          <motion.img
+            src={isoLogo}
+            alt='Flor Carpanetto'
+            className='isologo'
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 2 }}
+          />
 
-        <div className='contactContainer'>
-          <a href='https://wa.me/543516618383' target='_blank'><img src={whatsappLogo} className='logoSocialMedia' alt='logo' /></a>
-          <a href='https://www.linkedin.com/in/florcarpanetto' target='_blank'><img src={liknedinLogo} className='logoSocialMedia' alt='logo' /></a>
-          <a href='https://github.com/florcarpanetto' target='_blank'><img src={githubLogo} className='logoSocialMedia' alt='logo' /></a>
-          <a href='mailto:florcarpanetto@gmail.com' target='_blank'><img src={mailtoLogo} className='logoSocialMedia' alt='logo' /></a>
-        </div>
+          <div className='contactContainer'>
+            <motion.a
+              href='https://wa.me/543516618383'
+              target='_blank'
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 2 }}
+            >
+              <img src={whatsappLogo} className='logoSocialMedia' alt='logo' />
+            </motion.a>
+            <motion.a
+              href='https://www.linkedin.com/in/florcarpanetto'
+              target='_blank'
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 2 }}
+            >
+              <img src={liknedinLogo} className='logoSocialMedia' alt='logo' />
+            </motion.a>
+            <motion.a
+              href='https://github.com/florcarpanetto'
+              target='_blank'
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 2 }}
+            >
+              <img src={githubLogo} className='logoSocialMedia' alt='logo' />
+            </motion.a>
+            <motion.a
+              href='mailto:florcarpanetto@gmail.com'
+              target='_blank'
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 2 }}
+            >
+              <img src={mailtoLogo} className='logoSocialMedia' alt='logo' />
+            </motion.a>
+          </div>
         </div>
 
       </header>
@@ -39,7 +95,13 @@ function App() {
 
       <section>
         <div className='webs-container'>
-          <h2>WEB DEVELOPMENT</h2>
+          <motion.h2
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+          >
+            WEB DEVELOPMENT
+          </motion.h2>
           <WebCard link='http://astillerotecno.com.ar/' image={tecnoScreen} titlte='Shipyard: Tecno' />
           {/* <WebCard link='https://funny-gnome-218ccb.netlify.app/' image={hotelScreen} titlte='Giphos app' /> */}
           <WebCard link='https://funny-gnome-218ccb.netlify.app/' image={hotelScreen} titlte='Hotels Reservation' />
--- src/components/webCard/WebCard.jsx
+++ src/components/webCard/WebCard.jsx
@@ -1,14 +1,33 @@
+import { motion } from 'framer-motion';
+import './WebCard.css';
+import dragIcon from '../../assets/drag-4.svg'
 
-import './WebCard.css'
-
-export const WebCard = ({titlte, image, link}) => {
+export const WebCard = ({ titlte, image, link }) => {
   return (
-    <a href={link}>
 
-    <div className='card-container'>
-        <h3>{titlte}</h3>
-        <img src={image} alt='web screen' className='screenshot'/>
-    </div>
-    </a>
+
+    <motion.div
+      className='card-container'
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 0.9 }}
+      transition={{ duration: 3 }}
+      drag
+      dragConstraints={{
+        top: -170,
+        left: -170,
+        right: 170,
+        bottom: 170,
+      }}
+    >
+      <h3>{titlte}</h3>
+      <motion.a href={link}>
+        <motion.img src={image} alt='web screen' className='screenshot' />
+      </motion.a>
+      <div className='card-bottom'>
+        <img className='card-bottom-image' src={dragIcon} />
+
+        {'drag me'}
+      </div>
+    </motion.div>
   )
 }
